97162a21bd8f07fe44c2695fff54a1b6207df22f
wangye <wangye@xjgreat.com>
2023-11-01 17:26:10 +0800

    前4节内容

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import Image from 'next/image';
 
 import Link from 'next/link';
 export default function Page() {
@@ -25,6 +26,21 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
